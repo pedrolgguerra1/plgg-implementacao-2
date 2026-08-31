4631433edc4c6ede5d19aa2815ab28c0f5ee0c11
Pedro <pguerrape@gmail.com>
2026-08-31 18:41:16 -0300

    ajeitando main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 int rodar_serial(Params p, const char *login);
 int rodar_pthreads1(Params p, const char *login);
 int rodar_pthreads2(Params p, const char *login);
+int rodar_openmp(Params p, const char *login);
 
 int main(int argc, char **argv) {
     Params p;
@@ -17,6 +18,7 @@ int main(int argc, char **argv) {
     if (rodar_serial(p, LOGIN) != 0) return 1;
     if (rodar_pthreads1(p, LOGIN) != 0) return 1;
     if (rodar_pthreads2(p, LOGIN) != 0) return 1;
+    if (rodar_openmp(p, LOGIN) != 0) return 1;
 
     return 0;
 }
